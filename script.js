54d3d06b7e2418a411c524cc7d35e038482ae20f
tuthoang <hoangt@bu.edu>
2018-01-11 12:47:32 -0500

    formatting/cards

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-var aboutPos = $('#about').offset().top;
-var proPos = $('#projects').offset().top;
-var contactPos = $('#contact').offset().top;
 
-  // This is a functions that scrolls to #{blah}link
-function goToByScroll(id){
-      // Remove "link" from the ID
-    id = id.replace("link", "");
-      // Scroll
-    $('html,body').animate({
-        scrollTop: $("#"+id).offset().top},
-        'slow');
-}
-
-$("nav > div> > ul > li > a").click(function(e) { 
-      // Prevent a page reload when a link is pressed
-    e.preventDefault(); 
-      // Call the scroll function
-    goToByScroll(this.id);           
-});
+ $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
